e69c7b9f39e988e99f443c16c618b707072582c4
xts400 <dennis@threadstack.co>
2017-10-12 11:44:13 +0300

    Add vehicle types list adapter

---
+++ ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/adapters/VehicleTypesAdapter.java
@@ -0,0 +1,106 @@
+package com.shadtaxi.shadtaxi.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.shadtaxi.shadtaxi.R;
+import com.shadtaxi.shadtaxi.models.VehicleType;
+import com.shadtaxi.shadtaxi.views.Txt;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dennis on 10/12/17.
+ */
+
+public class VehicleTypesAdapter extends RecyclerView.Adapter<VehicleTypesAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<VehicleType> vehicleTypeArrayList;
+    private ArrayList<Integer> selectedPositions = new ArrayList<>();
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private Txt txtVehicleType;
+        private ImageView imageViewVehicleIcon;
+        private LinearLayout layoutVehicleType;
+
+        public ViewHolder(View v) {
+            super(v);
+            txtVehicleType = (Txt) v.findViewById(R.id.txtVehicleType);
+            imageViewVehicleIcon = (ImageView) v.findViewById(R.id.imageViewVehicleType);
+            layoutVehicleType = (LinearLayout) v.findViewById(R.id.layoutVehicleType);
+        }
+    }
+
+    public VehicleTypesAdapter(Context context, ArrayList<VehicleType> vehicleTypeArrayList) {
+        this.context = context;
+        this.vehicleTypeArrayList = vehicleTypeArrayList;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.txtVehicleType.setText(vehicleTypeArrayList.get(position).getVehicle_type());
+        Glide.with(context).load(vehicleTypeArrayList.get(position).getVehicle_icon()).into(holder.imageViewVehicleIcon);
+
+        if (!selectedPositions.contains(position)) {
+            if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Boda")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_motorbike_not_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Tuk")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_tuktuk_not_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Salon")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_salon_not_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Matatu")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_matatu_not_selected);
+            }
+            holder.txtVehicleType.setTextColor(context.getResources().getColor(R.color.colorBlack));
+        } else {
+            if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Boda")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_motorbike_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Tuk")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_tuktuk_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Salon")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_salon_selected);
+            } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Matatu")) {
+                holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_matatu_selected);
+            }
+            holder.txtVehicleType.setTextColor(context.getResources().getColor(R.color.colorPrimary));
+        }
+        holder.layoutVehicleType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!selectedPositions.contains(position)) {
+                    selectedPositions.clear();
+                    selectedPositions.add(position);
+                    if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Boda")) {
+                        holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_motorbike_selected);
+                    } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Tuk")) {
+                        holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_tuktuk_selected);
+                    } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Salon")) {
+                        holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_salon_selected);
+                    } else if (vehicleTypeArrayList.get(position).getVehicle_type().contains("Matatu")) {
+                        holder.imageViewVehicleIcon.setImageResource(R.drawable.icon_matatu_selected);
+                    }
+                    holder.txtVehicleType.setTextColor(context.getResources().getColor(R.color.colorPrimary));
+                    notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_vehicle_types_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return vehicleTypeArrayList.size();
+    }
+}
