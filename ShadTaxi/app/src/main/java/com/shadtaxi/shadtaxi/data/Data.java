1226dd2605b22e998241e44b4ad7c89b9ef1e939
xts400 <dennis@threadstack.co>
2017-12-18 11:08:56 +0300

    Add test data

--- ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/data/Data.java
+++ ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/data/Data.java
@@ -2,6 +2,7 @@ package com.shadtaxi.shadtaxi.data;
 
 import com.shadtaxi.shadtaxi.R;
 import com.shadtaxi.shadtaxi.models.AvailableDriver;
+import com.shadtaxi.shadtaxi.models.Country;
 import com.shadtaxi.shadtaxi.models.History;
 import com.shadtaxi.shadtaxi.models.VehicleType;
 
@@ -51,4 +52,14 @@ public class Data {
 
         return histories;
     }
+
+    public ArrayList<Country> countryList() {
+        ArrayList<Country> countryArrayList = new ArrayList<>();
+        countryArrayList.add(new Country("Kenya", "+254", R.drawable.kenya));
+        countryArrayList.add(new Country("Uganda", "+256", R.drawable.uganda));
+        countryArrayList.add(new Country("Tanzania", "+255", R.drawable.tanzania));
+
+        return countryArrayList;
+    }
+
 }
