eb66ff749fc98567054fc39e78e95b2f966da5c2
xts400 <dennis@threadstack.co>
2018-02-02 17:34:23 +0300

    Add photo upload api

--- ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/constants/Constants.java
+++ ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/constants/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final String LOGIN_URL = BASE_URL + "/oauth/token";
     public static final String GET_USER = BASE_URL + "/api/user";
     public static final String GET_VEHICLE_TYPES = BASE_URL + "/api/app/load";
+    public static final String UPDATE_PHOTO = BASE_URL + "/api/user/upload";
     public static final String UPDATE_PROFILE = BASE_URL + "/api/user/update";
     public static final String BECOME_DRIVER = BASE_URL + "/api/driver";
     public static final String TOGGLE_USER = BASE_URL + "/api/user";
