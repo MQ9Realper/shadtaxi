cc644ad796d229b6738502a6c6b8aa7007f79389
xts400 <dennis@threadstack.co>
2018-02-05 16:04:27 +0300

    Add nearest driver data model

---
+++ ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/models/NearestDriver.java
@@ -0,0 +1,89 @@
+package com.shadtaxi.shadtaxi.models;
+
+/**
+ * Created by dennis on 11/8/17.
+ */
+
+public class NearestDriver {
+    private int id;
+    private String email;
+    private String name;
+    private String image_url;
+    private float rating;
+    private String distance;
+    private String latlong;
+    private String address;
+    private String time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getLatlong() {
+        return latlong;
+    }
+
+    public void setLatlong(String latlong) {
+        this.latlong = latlong;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
