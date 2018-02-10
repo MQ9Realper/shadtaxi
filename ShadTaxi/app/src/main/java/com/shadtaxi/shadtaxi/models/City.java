6d2b7970c64edfe64510e43edd21c553064fd123
xts400 <dennis@threadstack.co>
2018-02-10 10:12:29 +0300

    Add city data model

---
+++ ShadTaxi/app/src/main/java/com/shadtaxi/shadtaxi/models/City.java
@@ -0,0 +1,35 @@
+package com.shadtaxi.shadtaxi.models;
+
+/**
+ * Created by dennis on 2/10/18.
+ */
+
+public class City {
+    private int id;
+    private String name;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
